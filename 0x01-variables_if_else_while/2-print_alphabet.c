8dde9d09b15839ba050288e63d3d6967f2e3b3e9
Orwakijala <kijala67@gmail.com>
2022-10-27 01:46:49 +0300

    Commit Message

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - prints the alphabet in lowercase
+ * followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char low;
+
+	for (low = 'a'; low <= 'z'; low++)
+	{
+		putchar(low);
+	}
+		putchar('\n');
+
+	return (0);
+
+}
